f475533f4cd0b2b64b1c1b2f20003a5bcddd890a
yuzhicheng <yuzhicheng@innobuddy.com>
2017-12-14 12:59:32 +0800

    adjust rc format

--- src/common/func.js
+++ src/common/func.js
@@ -4,17 +4,18 @@ const shell = require('shelljs')
 const fuzzy = (list, keyword) => list.filter(item => new RegExp(keyword.split('').map(c => c.replace(/[.?*+^$[\]\\(){}|]/g, '\\$&')).join('.*'), 'i').test(item))
 
 const getFilteredPods = function (argv) {
+  argv.oc = argv.oc || {}
   const keyword = argv.keyword || argv.defaultKeyword
   let listPods = shell.exec('oc get pods --no-headers -o=custom-columns=NAME:.metadata.name', {
     silent: true
   })
 
   if (listPods.code > 0) {
-    const username = argv.username ? argv.username : readlineSync.question('Please input your oc username: ')
-    const password = argv.password ? argv.password : readlineSync.question('Please input your oc passworld: ', {
+    const username = argv.oc.username ? argv.oc.username : readlineSync.question('Please input your oc username: ')
+    const password = argv.oc.password ? argv.oc.password : readlineSync.question('Please input your oc passworld: ', {
       hideEchoBack: true
     })
-    const host = argv.host ? argv.host : ''
+    const host = argv.oc.host ? argv.oc.host : ''
     shell.exec(`oc login -u ${username} -p ${password} ${host}`)
     listPods = shell.exec('oc get pods --no-headers -o=custom-columns=NAME:.metadata.name', {
       silent: true
